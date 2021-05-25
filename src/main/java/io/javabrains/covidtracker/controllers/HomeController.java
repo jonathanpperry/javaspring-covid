fa91925889e076121e9210097898f99ba41bae48
Jonathan Perry <jpper@umich.edu>
2021-05-25 10:03:33 +0900

    Added sum of total cases and a diff from previous day for each country and total, added bootstrap and styled and rendered data out to page

--- src/main/java/io/javabrains/covidtracker/controllers/HomeController.java
+++ src/main/java/io/javabrains/covidtracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package io.javabrains.covidtracker.controllers;
 
+import io.javabrains.covidtracker.models.LocationStats;
 import io.javabrains.covidtracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -13,7 +16,14 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
         return "home";
     }
 }
